b3c6d15011a9252a684fa21e0112d067bc7f717c
SarahMarquardt73 <96892705+SarahMarquardt73@users.noreply.github.com>
2022-11-30 16:52:49 -0500

    Add files via upload

---
+++ project.py
@@ -0,0 +1,22 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("blue")
+screen.title("Drawing Lines Practice")
+
+#Draw a line
+my_turtle = turtle.Turtle()
+my_turtle.pensize(5)
+my_turtle.shape("circle")
+my_turtle.forward(100)
+
+#Practice drawing a loop
+for x in range(0,8):
+    my_turtle.forward(100)
+    my_turtle.backward(100)
+    my_turtle.left(45)
+
+my_turtle.hideturtle()
+turtle.done
+
+
